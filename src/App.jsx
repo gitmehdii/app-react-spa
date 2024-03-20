8bc0991fd28da0b7e17612e0f0731fee18f42216
MDI <mehdi.azouz@epita.fr>
2024-03-20 09:32:44 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './Petite.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
+import Navbar from "./pages/Navbar";
+import Footer from "./pages/Footer";
+
+function App() {
+    return (
+        <Router >
+            <Navbar />
+            <main>
+                {/* Place non-global routes inside an HTML <main> tag */}
+                <Routes>
+                    {/* Use Home component for the root path */}
+                    <Route index element={<Home />} />
+                    <Route path="products" element={<Products />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Routes>
+            </main>
+            <Footer />
+        </Router>
+    );
+}
+
+export default App;
